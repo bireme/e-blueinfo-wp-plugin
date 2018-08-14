6c3f11ecfde790936189a4fa06b04cb4b25075e7
Wilson Moura <wilsonsmoura@gmail.com>
2018-08-14 16:54:56 -0300

    Update memoria-azul.php

--- memoria-azul.php
+++ memoria-azul.php
@@ -232,24 +232,17 @@ if(!class_exists('Memoria_Azul_Plugin')) {
 
 		    // check if is defined GA code and pagename starts with plugin slug
 		    if ($plugin_config['google_analytics_code'] != ''
-		        && strpos($pagename, $this->plugin_slug) === 0){
+		        && strpos($pagename, $this->plugin_slug) === 0) {
 
 		?>
-
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', '<?php echo $plugin_config['google_analytics_code'] ?>']);
-		  _gaq.push(['_setCookiePath', '/<?php echo $plugin_config['$this->plugin_slug'] ?>']);
-		  _gaq.push(['_trackPageview']);
-
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-
-		</script>
-
+        <script type="text/javascript">
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','__gaTracker');
+            __gaTracker('create', '<?php echo $plugin_config['google_analytics_code']; ?>', 'auto');
+            __gaTracker('send', 'pageview');
+        </script>
 		<?php
 		    } //endif
 		}
